372dd6ec6225758988aefc83085f3d9554143ee1
statkevich <statkevich.ivan9999@gmail.com>
2024-06-02 17:24:56 +0300

    Add chat message subscription
    This commit introduces a message subscription to the chat. The graphql schema now includes a `syncMessagesSubscription` under 'Subscription' type. Errors during the publish and subscription process are handled with `InternalServerErrorException`.

--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -1,11 +1,13 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
-import { ClassSerializerInterceptor, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver, Subscription } from '@nestjs/graphql';
+import { ClassSerializerInterceptor, InternalServerErrorException, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { User } from '../users/user.entity';
 import { PubSub } from 'graphql-subscriptions';
 import { CreateMessageRequestDTO } from './dto/create-message-request.dto';
 import { ChatService } from './chat.service';
 import { MessageResponseDTO } from './dto/message-response.dto';
+import { InvitePlayersResponseDTO } from '../game/dto/invite-players-response.dto';
+import { Id } from '../common.types';
 
 
 const pubSub = new PubSub();
@@ -25,7 +27,32 @@ export class ChatResolver {
     @Args('createMessageInput') createMessageInput: CreateMessageRequestDTO,
     @Context() context: { req: Request },
   ): Promise<MessageResponseDTO> {
-    const user = context.req['user'] as User;
-    return this.chatService.createMessage(createMessageInput, user);
+    const user: User = context.req['user'] as User;
+    const message: MessageResponseDTO = await this.chatService.createMessage(createMessageInput, user);
+
+    try {
+      pubSub.publish('syncMessages', message);
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+
+    return message;
+  }
+
+  @Subscription(() => MessageResponseDTO, {
+    nullable: true,
+    resolve: (value) => value,
+    filter: (payload, variables) => {
+      return true;
+    },
+  })
+  syncMessagesSubscription(
+    @Args('gameId') gameId: Id,
+  ) {
+    try {
+      return pubSub.asyncIterator('syncMessages');
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 }
